bb67f94f23266710c255ee628d1ff2fe6187b032
Alé <101077243+akathm@users.noreply.github.com>
2024-06-25 18:45:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import numpy as np
 import altair as alt
 import requests
 from io import StringIO
+from datetime import datetime, timedelta
 
 st.set_page_config(page_title='KYC Lookup Tool', page_icon='🗝️')
 st.title('🗝️ KYC Lookup Tool')
