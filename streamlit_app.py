74ca8b8331ab9958eb60c145c7c09311db8c0f13
Alé <101077243+akathm@users.noreply.github.com>
2024-07-01 12:00:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -222,8 +222,8 @@ def main():
         if 'avatar' not in persons_df.columns:
             persons_df['avatar'] = ''
         if search_term:
-            search_and_display(persons_df, inquiries_df, search_term, ['avatar', 'email', 'l2_address', 'updated_at', 'status'], 
-                               "This contributor is {status} for KYC.")
+            search_and_display(persons_df, inquiries_df, search_term, "This contributor is {status} for KYC.", ['avatar', 'email', 'l2_address', 'updated_at', 'status'] 
+                               )
     elif option == 'Grants Round':
         form_df['grant_id'] = form_df['grant_id'].astype(str)
         projects_df['grant_id'] = projects_df['grant_id'].astype(str)
