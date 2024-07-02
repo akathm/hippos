15466499a21db6ddd16248a35c9c8798c25f34c3
Alé <101077243+akathm@users.noreply.github.com>
2024-07-02 02:59:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,18 +165,10 @@ def main():
     form_df = fetch_csv(owner, repo, form_path, access_token)
 
     if persons_df is not None and 'updated_at' in persons_df.columns:
-        try:
-            persons_df['updated_at'] = pd.to_datetime(persons_df['updated_at'], utc=True)
-        except Exception as e:
-            st.error(f"Error converting 'updated_at' to datetime: {e}")
-            st.stop()
+        persons_df['updated_at'] = pd.to_datetime(persons_df['updated_at'], utc=True)
 
     if businesses_df is not None and 'updated_at' in businesses_df.columns:
-        try:
-            businesses_df['updated_at'] = pd.to_datetime(businesses_df['updated_at'], utc=True)
-        except Exception as e:
-            st.error(f"Error converting 'updated_at' to datetime: {e}")
-            st.stop()
+        businesses_df['updated_at'] = pd.to_datetime(businesses_df['updated_at'], utc=True)
 
     if inquiries_df is not None and 'updated_at' in inquiries_df.columns:
         inquiries_df['updated_at'] = pd.to_datetime(inquiries_df['updated_at'], utc=True)
