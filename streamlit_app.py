269225f589e7663e2d99a0e173c848aa7baba112
Alé <101077243+akathm@users.noreply.github.com>
2024-08-01 10:46:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -324,7 +324,7 @@ def main():
         filtered_df = merged_df[merged_df['project_name'].isin(projects_selection)] if projects_selection else merged_df
 
     st.write(filtered_df)
-    st.write(cases_df)
+    st.write(inquiries_df)
         
 ## Grants Rounds--------------------------------------------
         
