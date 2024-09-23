7f893c3531667a33f59cd51db35781daa18da6ba
Alé <101077243+akathm@users.noreply.github.com>
2024-09-23 15:23:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,6 +292,28 @@ def main():
         if search_term:
             search_and_display(all_contributors, search_term, ['avatar', 'email', 'l2_address', 'updated_at', 'status'], 
                                "This contributor is {status} for KYC.", status_column='status', date_column='updated_at')
+    elif option == 'Grants Round':
+        if form_entries.empty:
+            st.write("Grants round not started")
+        else:
+            kyc_columns = [f'kyc_email{i}' for i in range(10)]
+            kyb_columns = [f'kyb_email{i}' for i in range(5)]
+            
+            kyc_emails = typeform_data.melt(id_vars=['project_id'], value_vars=kyc_columns, var_name='kyc_email_num', value_name='kyc_email')
+            kyb_emails = typeform_data.melt(id_vars=['project_id'], value_vars=kyb_columns, var_name='kyb_email_num', value_name='kyb_email')
+
+            kyc_emails['status'] = kyc_emails['kyc_email'].apply(get_kyc_status)
+            kyb_emails['status'] = kyb_emails['kyb_email'].apply(get_kyb_status)
+
+            kyc_emails = kyc_emails[kyc_emails['kyc_email'].notna()]
+            kyb_emails = kyb_emails[kyb_emails['kyb_email'].notna()]
+
+            st.write("###Status for Responsible Individuals (KYC)")
+            st.write(kyc_emails[['project_id', 'kyc_email', 'status']])
+
+            st.write("###Status for Responsible Businesses (KYB)")
+            st.write(kyb_emails[['project_id', 'kyb_email', 'status']])
+
 
 ## TESTING--------------------------------------------------
     
