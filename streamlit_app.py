9e80346aa258c7bf40d7de0d3c8b386c7a26f9ad
Alé <101077243+akathm@users.noreply.github.com>
2024-09-23 19:32:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -378,6 +378,9 @@ def main():
     
         kyc_emails = merged_df[merged_df['kyc_email0'].notnull()]['kyc_email0'].unique()
         kyb_emails = merged_df[merged_df['kyb_email0'].notnull()]['kyb_email0'].unique()
+
+        print(merged_df.columns)
+        print(projects_df)
     
         kyc_results = []
         for email in kyc_emails:
