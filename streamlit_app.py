a19cc6f1dacd709944c5e7a2907d1a27116bc27e
Alé <101077243+akathm@users.noreply.github.com>
2024-10-14 15:58:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -530,7 +530,7 @@ def main():
 
 ## TESTING--------------------------------------------------
 
-st.write(typeform_data)
+    st.write(typeform_data)
 
 ##st.write(projects_df)
 ##st.write(all_projects)
